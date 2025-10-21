628dde8347758d207a94cf5c9e7964a277b73ff8
DinoLabs Tech Services <olaoluwadaniel1091@gmail.com>
2025-10-21 19:37:54 +0100

    style: Adjust viewport initial-scale for improved responsiveness
    Changed the `initial-scale` value in the viewport meta tag from `0.8` to `1.0`. This ensures the webpage renders at its natural 100% scale on initial load, preventing unintended zoom-out and providing a consistent, optimal viewing experience across various devices.

--- components/header.php
+++ components/header.php
@@ -13,15 +13,15 @@
           <li><a href="index.php">Home</a></li>
           <li><a href="students-life.php">Students Life</a></li>
           <li><a href="events.php">Events</a></li>
-          <li><a href="alumni.php">Alumni</a></li>
           <li><a href="gallery.php">Gallery</a></li>
 
-           <li class="dropdown"><a href="#"><span>More Pages</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+          <li class="dropdown"><a href="#"><span>Our School</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
             <ul>
               <li><a href="admissions.php">Admissions</a></li>
               <li><a href="academics.php">Academics</a></li>
               <li><a href="faculty-staff.php">Faculty &amp; Staff</a></li>
               <li><a href="campus-facilities.php">Campus &amp; Facilities</a></li>
+              <li><a href="alumni.php">Alumni</a></li>
             </ul>
           </li>
           <!-- <li class="dropdown"><a href="#"><span>More Pages</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
@@ -44,32 +44,32 @@
                   <li><a href="dashboard.php">Blog Dashboard</a></li>
                 </ul>
               <? } else { ?>
-              
+
+              <?php } ?>
+
+            <?php } else { ?>
+              <a href="blog.php">Blog</a>
             <?php } ?>
 
-          <?php } else { ?>
-            <a href="blog.php">Blog</a>
-          <?php } ?>
+              </li>
 
-          </li>
+              <?php if (isset($_SESSION['role']) == 'Superuser') { ?>
+                <li><a href="backend/superdashboard.php">Dashboard</a></li>
+              <?php } elseif (isset($_SESSION['role']) == 'Administrator' || isset($_SESSION['role']) == 'Teacher' || isset($_SESSION['role']) == 'Admmission') { ?>
+                <li><a href="backend/dashboard.php">Dashboard</a></li>
+              <?php } elseif (isset($_SESSION['role']) == 'Tuckshop') { ?>
+                <li><a href="backend/tuckdashboard.php">Dashboard</a></li>
+              <?php } elseif (isset($_SESSION['role']) == 'Parent') { ?>
+                <li><a href="backend/parent.php">Dashboard</a></li>
+              <?php } elseif (isset($_SESSION['role']) == 'Bursary') { ?>
+                li><a href="backend/account/index.php">Dashboard</a></li>
+              <?php } else { ?>
+                <li><a href="backend/login.php">Dashboard</a></li>
+              <?php }
+              ?>
 
-          <?php if (isset($_SESSION['role']) == 'Superuser') { ?>
-            <li><a href="backend/superdashboard.php">Dashboard</a></li>
-          <?php } elseif (isset($_SESSION['role']) == 'Administrator' || isset($_SESSION['role']) == 'Teacher' || isset($_SESSION['role']) == 'Admmission') { ?>
-            <li><a href="backend/dashboard.php">Dashboard</a></li>
-          <?php } elseif (isset($_SESSION['role']) == 'Tuckshop') { ?>
-            <li><a href="backend/tuckdashboard.php">Dashboard</a></li>
-          <?php } elseif (isset($_SESSION['role']) == 'Parent') { ?>
-            <li><a href="backend/parent.php">Dashboard</a></li>
-          <?php } elseif (isset($_SESSION['role']) == 'Bursary') { ?>
-            li><a href="backend/account/index.php">Dashboard</a></li>
-          <?php } else { ?>
-            <li><a href="backend/login.php">Dashboard</a></li>
-          <?php }
-          ?>
-
-          <li><a href="about.php">About</a></li>
-          <li><a href="contact.php">Contact</a></li>
+              <li><a href="about.php">About</a></li>
+              <li><a href="contact.php">Contact</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
