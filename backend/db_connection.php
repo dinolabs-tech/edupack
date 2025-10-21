70aecc6fdc28a3d2c30dfbec3241635e32b977a2
DinoLabs Tech Services <olaoluwadaniel1091@gmail.com>
2025-10-21 21:19:47 +0100

    switched to online connection

--- backend/db_connection.php
+++ backend/db_connection.php
@@ -1,23 +1,23 @@
 <?php
 // onlice connection
-// $servername = "localhost";
-// $username = "dinolabs_root";
-// $password = "foxtrot2november";
-// $dbname = "dinolabs_edupack";
-// $conn = new mysqli($servername, $username, $password, $dbname);
-
-// if ($conn->connect_error) {
-//     die("Connection failed: " . $conn->connect_error);
-// }
-
-// offline connection
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "edupack";
+$username = "dinolabs_root";
+$password = "foxtrot2november";
+$dbname = "dinolabs_edupack";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+// offline connection
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "edupack";
+// $conn = new mysqli($servername, $username, $password, $dbname);
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 ?>
